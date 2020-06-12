36826ef1b47536b5435c025eee7d7c56946cbd49
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-06-12 07:12:11 -0700

    Test is passing maze with 997 moves

--- adv.py
+++ adv.py
@@ -12,11 +12,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -59,7 +59,7 @@ print('CURRENT EXITS: ', player.current_room.get_exits())
 print('CURRENT EXITS STRING: ', player.current_room.get_exits_string())
 print('Map File Length: ', len(room_graph))
 print('Traversal Path Length: ', len(traversal_path)) 
-print('Starting Journal: ', journal)
+# print('Starting Journal: ', journal)
 print('\n')
 
 my_visited_rooms.add(player.current_room.id)
@@ -70,10 +70,10 @@ my_visited_rooms.add(player.current_room.id)
 
 while len(my_visited_rooms) < len(room_graph):
 
-    
-
-
+    # Get current Room ID
     first_room = player.current_room.id
+
+    backtrack = False
 
     # Fill in NONE Directions
     first_room_directions = player.current_room.get_exits()
@@ -81,33 +81,53 @@ while len(my_visited_rooms) < len(room_graph):
         if direction not in first_room_directions:
             journal[first_room][direction] = None
 
-    # If any unknown rooms:
 
+    # Check which rooms we dont know.
+    num_known_rooms = 0
     for direction in journal[first_room]:
-        if journal[first_room][direction] == '?':
-            print('yes There is an uknown room!')
+        if journal[first_room][direction] != '?':
+            num_known_rooms += 1
+        # if a room is unknown
+        elif not None:
+            move_to_room = direction
+    # If all rooms are known
+    if num_known_rooms == 4:        
+        backtrack = True
 
-        # if journal[first_room]['n'] == '?':
-            # Set backtrack to False
-            backtrack = False
-            # Add to Stack
-            s.push(direction)
-            # Add to traversal path
-            traversal_path.append(direction)
-            # Go There
-            player.travel(direction)
-            # fill in journal 
-            second_room = player.current_room.id
-            journal[first_room][direction] = second_room
-            opposite_dir = get_opposite(direction)
-            journal[second_room][opposite_dir] = first_room
-            # add to my visited rooms
-            my_visited_rooms.add(second_room)
+    # Start Backtracking
+    if backtrack is True:
+        last_move = s.pop()
+        move_to_room = get_opposite(last_move)
+        # Add to traversal path Where i am about to Go
+        traversal_path.append(move_to_room)
+        # Go There
+        player.travel(move_to_room)
 
-print('Final Journal: ', journal)
+    # If i should NOT backtrack
+    else:
+        print(f'Room {first_room} Directions: ', journal[first_room])
+        print(f'Moving from room {first_room} in direction {move_to_room}')
+        # Add to Stack where I am about to Go
+        s.push(move_to_room)
+        # Add to traversal path Where i am about to Go
+        traversal_path.append(move_to_room)
+        # Go There
+        player.travel(move_to_room)
+        # fill in journal For first and second room
+        second_room = player.current_room.id
+        journal[first_room][move_to_room] = second_room
+        opposite_dir = get_opposite(move_to_room)
+        journal[second_room][opposite_dir] = first_room
+        # add to my visited rooms
+        my_visited_rooms.add(second_room)
+    
+
+
+
+# print('Final Journal: ', journal)
 print('My visited rooms length: ', len(my_visited_rooms))
 print('Map file length: ', len(room_graph))
-print('Eding ROOM ID: ', player.current_room.id)
+print('Ending ROOM ID: ', player.current_room.id)
 
 
 
